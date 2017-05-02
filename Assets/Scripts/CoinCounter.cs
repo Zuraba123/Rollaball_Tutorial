bcd620f3c7e4c50e7092a5fb56b5bcb7297371de
kasradzenika <kasradzenika@yahoo.com>
2017-05-02 20:51:49 +0400

    added coins to the game

---
+++ Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCounter : MonoBehaviour
+{
+    public int score = 0;
+    public Text scoreText;
+    public string coinTagString;
+    public string badCoinTagString;
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Collision " + col.gameObject.name);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == coinTagString)
+        {
+            score = score + 1;
+        }
+        else if (other.tag == badCoinTagString && score > 0)
+        {
+            score = score - 1;
+        }
+
+        Debug.Log("Triggered " + score + " " + other.gameObject.name);
+
+        scoreText.text = "Score: " + score;
+
+        Destroy(other.gameObject);
+    }
+}
