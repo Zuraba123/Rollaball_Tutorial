0dee79cd0be25f64f9b9899b0683df07ba0bb607
kasradzenika <kasradzenika@yahoo.com>
2017-05-04 20:35:16 +0400

    added menu, final commit

--- Assets/Scripts/CoinCounter.cs
+++ Assets/Scripts/CoinCounter.cs
@@ -7,6 +7,23 @@ public class CoinCounter : MonoBehaviour
     public Text scoreText;
     public string coinTagString;
     public string badCoinTagString;
+    
+    public Text resultText;
+    public string winString;
+    public string loseString;
+
+    public GameObject menu;
+    public PlayerMover myMover;
+    public Rigidbody myBody;
+
+    int targetScore;
+
+    void Start()
+    {
+        targetScore = GameObject.FindGameObjectsWithTag(coinTagString).Length;
+
+        menu.SetActive(false);
+    }
 
     void OnCollisionEnter(Collision col)
     {
@@ -19,15 +36,36 @@ public class CoinCounter : MonoBehaviour
         {
             score = score + 1;
         }
-        else if (other.tag == badCoinTagString && score > 0)
+        else if (other.tag == badCoinTagString)
         {
-            score = score - 1;
+            GameOver(false);
         }
 
         Debug.Log("Triggered " + score + " " + other.gameObject.name);
 
         scoreText.text = "Score: " + score;
 
+        if (targetScore == score)
+        {
+            GameOver(true);
+        }
+
         Destroy(other.gameObject);
+    }
+
+    void GameOver(bool didWin)
+    {
+        menu.SetActive(true);
+        myMover.enabled = false;
+        myBody.isKinematic = true;
+
+        if (didWin)
+        {
+            resultText.text = winString;
+        }
+        else
+        {
+            resultText.text = loseString;
+        }
     }
 }
